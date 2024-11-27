4b64b02f8990750fa30e88dc20d130be4b2a9f3b
Caio <caio.vr12@gmail.com>
2024-11-27 07:10:46 -0300

    Finalizando layout do componente Preview

--- src/components/main/preview/Preview.tsx
+++ src/components/main/preview/Preview.tsx
@@ -9,9 +9,15 @@ export const Preview = () => {
   return(
     <div className={`${styles.wrap}`}>
       <section className={`${styles.block} ${styles.blockLeft}`}>
+        <a className={styles.aFloatLeft}>
+          Fluxo
+        </a>
         <PaymentsCards/>
       </section>
       <section className={`${styles.block} ${styles.blockRight}`}>
+        <a className={`${styles.aFloatLeft} ${styles.aFloatLeftRight}`}>
+          Orçamentos
+        </a>
         <Works/>
       </section>
     </div>
